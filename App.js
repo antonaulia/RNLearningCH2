1f649555d5a4ee4ce3e4ed422ea58d2685676f11
antonaulia <antoniusferiantoaulia@gmail.com>
2022-04-07 12:10:05 +0700

    goal list handling input

--- App.js
+++ App.js
@@ -1,29 +1,53 @@
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { color } from 'react-native/Libraries/Components/View/ReactNativeStyleAttributes';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [inputGoal,setInputGoal] =useState('')
+  function goalInputHandler(inputGoal){
+    setInputGoal(inputGoal)
+  }
+  const [courseGoalsArray, setCourseGoalsArray]= useState([])
+  function addButtonHandler(){
+    setCourseGoalsArray([...courseGoalsArray,inputGoal])
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello world ! </Text>
-      <Text style={styles.textbox}> 
-        Another Piece of text ! asd
-      </Text>
-      <Button title='OK !' />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder='Your New Goal Here !' onChangeText={goalInputHandler}/>
+        <Button title='Add Goal' onPress={addButtonHandler}/>
+      </View>
+      <View style={styles.goalList}>
+        <Text>List Of Goals :</Text>
+        {courseGoalsArray.map((goal)=><Text>{goal}</Text>)}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  appContainer :{
+    flex : 1,
+    paddingTop: 50,
+    paddingHorizontal : 16
   },
-  textbox: {
-    margin: 16, 
-    borderWidth: 2, 
-    borderColor: 'red', 
-    padding: 16 
+  inputContainer:{
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems:'center',
+    marginBottom: 24,
+    borderBottomColor:'grey',
+    borderBottomWidth : 1
+  },
+  textInput:{
+    borderWidth:1,
+    borderColor: 'grey',
+    width: '70%',
+    marginRight: 8,
+    padding: 8
+  },
+  goalList:{
+    flex: 6
   }
 });
